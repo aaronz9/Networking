c10735656e95585a575446dccecb3b6794daccc5
Aaron Fernandez <35059964+aaronz9@users.noreply.github.com>
2018-10-06 16:31:57 +0530

    Update mac_change_final.py

--- mac_change_final.py
+++ mac_change_final.py
@@ -1,9 +1,27 @@
+
+#; in linux is used to tell terminal that we want to execute another command after current
+
 import subprocess #module name for netowrking
 
-subprocess.call("ifconfig wlp6s0 down", shell = True)  #shell = true so we run linux commands
-subprocess.call("ifconfig wlp6s0 hw ether  34:e6:ad:92:e0:3f", shell = True)
-subprocess.call("ifconfig wlp6s0 up", shell = True)
+interface = input("Interface")
+new_mac = input("New Mac address")
 
+print("[+] changing MAC address for"+" "+interface +"to"+" "+new_mac)
+
+
+#subprocess.call("ifconfig" + interface+  "down", shell = True)  #shell = true so we run linux commands
+#subprocess.call("ifconfig"+ interface+ "hw ether"+ new_mac, shell = True)
+#subprocess.call("ifconfig" + interface+ "up", shell = True)
+
+#more secure vesion of commands than above
+subprocess.call(["ifconfig",interface,"down"]) #instead of a string we can use a list
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
+
+
+
+#note my interface is different from urs choose your own interface whos mac you want to change
+#
 
 
 #note my interface is different from urs choose your own interface whos mac you want to change
